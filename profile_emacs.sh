9a98d397be295123c819c853ced938f1ea67ef7e
PalaceChan <XXX>
2023-03-25 10:53:11 -0500

    support profiling packages too

--- profile_emacs.sh
+++ profile_emacs.sh
@@ -2,8 +2,15 @@
 
 set -o errexit -o nounset -o pipefail
 
-emacs -Q \
-      --eval "(profiler-start 'cpu)" \
-      -l ~/.emacs.d/init.el \
-      -f profiler-stop \
-      -f profiler-report
+if [[ $1 == "profiler" ]]; then
+    emacs -Q \
+          --eval "(profiler-start 'cpu)" \
+          -l ~/.emacs.d/init.el \
+          -f profiler-stop \
+          -f profiler-report
+elif [[ $1 == "packages" ]]; then
+    emacs -Q \
+          --eval '(setq use-package-compute-statistics t)' \
+          -l ~/.emacs.d/init.el \
+          -f use-package-report
+fi
